19fae8a819be7fc4a0e3c022aa60b9bf28e84ddd
Dennis Ideler <ideler.dennis@gmail.com>
2023-02-09 23:46:03 +0000

    Test page title

--- src/routes/lists/+page.svelte
+++ src/routes/lists/+page.svelte
@@ -1,1 +1,6 @@
+<svelte:head>
+	<title>Lists</title>
+	<meta name="description" content="Your Grocery Lists" />
+</svelte:head>
+
 <h1>Lists</h1>
--- tests/lists.test.ts
+++ tests/lists.test.ts
@@ -2,5 +2,6 @@ import { expect, test } from '@playwright/test';
 
 test('lists page has expected title', async ({ page }) => {
 	await page.goto('/lists');
+	await expect(page).toHaveTitle(/Lists/);
 	expect(await page.textContent('h1')).toBe('Lists');
 });
