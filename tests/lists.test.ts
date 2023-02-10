6deafb970a57ed547a8ca109265dcf85e55b9451
Dennis Ideler <ideler.dennis@gmail.com>
2023-02-10 00:00:24 +0000

    Stop using deprecated playwright API
    https://playwright.dev/docs/api/class-page#page-text-content

--- tests/lists.test.ts
+++ tests/lists.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
-test('lists page has expected title', async ({ page }) => {
+test('lists page shows various titles', async ({ page }) => {
 	await page.goto('/lists');
 	await expect(page).toHaveTitle(/Lists/);
-	expect(await page.textContent('h1')).toBe('Lists');
+	await expect(page.locator('h1')).toContainText('Lists')
 });
