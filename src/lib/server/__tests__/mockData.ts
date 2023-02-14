7b4d1400fc4432ee16f04929c9351752d53eddb9
Dennis Ideler <ideler.dennis@gmail.com>
2023-02-14 01:16:43 +0000

    Refactor mock data functions and add new ones
    - newList now accepts any list prop, not just title and items
    - newLists for creation of multiple lists at once
    - listsToMap for converting to the ListStore format for (bulk) storage

--- src/lib/server/__tests__/mockData.ts
+++ src/lib/server/__tests__/mockData.ts
@@ -1,19 +1,33 @@
 import { randomUUID } from 'crypto';
-import type { List, Item } from '$lib/types';
+import type { ListStore, List, Item } from '$lib/types';
 
-export const newList = ({
-	title = 'Groceries',
-	items = [
-		{ done: false, desc: 'Eggs' },
-		{ done: true, desc: 'Ham' },
-	] as Item[],
-} = {}): List => {
+type ListProps = {
+	id?: string;
+	title?: string;
+	items?: Item[];
+	created_at?: string;
+	updated_at?: string;
+};
+
+export const listsToMap = (lists: Array<List>): ListStore => {
+	return lists.reduce((acc, list) => ({ ...acc, [list.id]: list }), {});
+};
+
+export const newLists = (listsProps: Array<ListProps>) => {
+	return listsProps.map((props) => newList(props));
+};
+
+export const newList = (data?: ListProps): List => {
 	const currentTime = new Date().toISOString();
-	return {
+	const defaults = {
 		id: randomUUID(),
-		title,
-		items,
+		title: 'Groceries',
+		items: [
+			{ done: false, desc: 'Eggs' },
+			{ done: true, desc: 'Ham' },
+		],
 		created_at: currentTime,
 		updated_at: currentTime,
 	};
+	return { ...defaults, ...data };
 };
