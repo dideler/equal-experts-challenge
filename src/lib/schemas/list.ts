d16efcfd27a3a4d4ca7fe84d4af4964dd96f548f
Dennis Ideler <ideler.dennis@gmail.com>
2023-02-21 06:11:15 +0000

    Fix `updated_at` not being updated on save
    Because the schema was defined by calling the date function, the value
    was cached rather than re-computed. We now pass it a function without
    calling it, so it gives us the current time when using the default.

--- src/lib/schemas/list.ts
+++ src/lib/schemas/list.ts
@@ -4,6 +4,8 @@ import type { List, FormList, FormDataRow } from '$lib/types';
 
 export { ZodError as ValidationError } from 'zod';
 
+const currentTime = () => new Date().toISOString();
+
 export const itemSchema = z.object({
 	desc: z.string().trim(),
 	done: z.coerce.boolean().default(false),
@@ -13,8 +15,8 @@ export const listSchema = z.object({
 	id: z.string().uuid().default(randomUUID),
 	title: z.string().trim().min(1),
 	items: z.array(itemSchema).default([]),
-	created_at: z.string().datetime().default(new Date().toISOString()),
-	updated_at: z.string().datetime().default(new Date().toISOString()),
+	created_at: z.string().datetime().default(currentTime),
+	updated_at: z.string().datetime().default(currentTime),
 });
 
 const formId = z.tuple([z.string().regex(/^id$/), z.string().uuid()]);
