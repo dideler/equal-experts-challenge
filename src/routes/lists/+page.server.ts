e1d86b5b2ce4e38357d1a639a1cde8b844934cf5
Dennis Ideler <ideler.dennis@gmail.com>
2023-02-21 06:14:10 +0000

    Improve error handling and responses of lists/ API
    Makes it more consistent with the patterns defined on the /list/:id API.

--- src/routes/lists/+page.server.ts
+++ src/routes/lists/+page.server.ts
@@ -2,36 +2,39 @@ import * as db from '$lib/server/database';
 import { listSchema, ValidationError } from '$lib/schemas/list';
 import { error, fail } from '@sveltejs/kit';
 import type { Actions, PageServerLoad, RequestEvent } from './$types';
-import type { List } from '$lib/types';
 
 export const load = (async () => {
 	return {
 		lists: await db.getLists(),
+		error: null,
 	};
 }) satisfies PageServerLoad;
 
 export const actions = {
-	create: async (event: RequestEvent) => {
-		const formData = await event.request.formData();
-		const data = Object.fromEntries(formData);
-		let list: List;
+	create: async ({ request }: RequestEvent) => {
+		let list;
 
 		try {
+			const formData = await request.formData();
+			const data = Object.fromEntries(formData);
+
 			list = listSchema.parse(data);
 			list = await db.saveList(list);
-			return { action: 'create', list };
+
+			return { action: 'create', list, error: null };
 		} catch (err) {
 			console.error('Error POST /lists?/create', { error: err });
+
 			if (err instanceof ValidationError) {
 				return fail(422, {
 					action: 'create',
-					data,
+					list,
 					error: 'Validation failed',
 					errors: err.flatten().fieldErrors,
 				});
-			} else {
-				throw error(400, 'Failed to create list');
 			}
+
+			throw error(400, 'Failed to create list');
 		}
 	},
 } satisfies Actions;
