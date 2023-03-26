9d27270f426671dd50288b92bea8f9aa5e2c10d5
Dennis Ideler <ideler.dennis@gmail.com>
2023-03-26 22:30:29 +0100

    Style /list/:id page
    Going for a basic minimal look, inspired by Google Keep.
    
    In hindsight I should have designed the list to open in the foreground
    of the /lists page, instead of rendering on its own page, while
    still being identifiable by a unique route. The way it's done now
    makes the page feel empty since most lists will be small.

--- src/lib/components/List.svelte
+++ src/lib/components/List.svelte
@@ -40,70 +40,108 @@
 	};
 </script>
 
-<form method="POST">
-	<input
-		type="hidden"
-		name="id"
-		value={id}
-		aria-label="List ID"
-		aria-hidden="true"
-	/>
-	<input
-		type="hidden"
-		name="created-at"
-		value={created_at}
-		aria-label="List created at"
-	/>
-	<input
-		disabled
-		type="hidden"
-		name="updated-at"
-		value={updated_at}
-		aria-label="List last saved at"
-	/>
+<div class="min-h-screen bg-base-200">
+	<form method="POST">
+		<div class="form-control">
+			<div class="card card-bordered w-96">
+				<div class="card-body">
+					<input
+						type="hidden"
+						name="id"
+						value={id}
+						aria-label="List ID"
+						aria-hidden="true"
+					/>
+					<input
+						type="hidden"
+						name="created-at"
+						value={created_at}
+						aria-label="List created at"
+					/>
+					<input
+						disabled
+						type="hidden"
+						name="updated-at"
+						value={updated_at}
+						aria-label="List last saved at"
+					/>
 
-	<input
-		id="input-title"
-		data-testid="input-title"
-		type="text"
-		name="title"
-		placeholder="Title"
-		bind:value={title}
-	/>
-	{#each items as { done, desc }, i}
-		<div class="item">
-			<input
-				data-testid="input-item-check"
-				name={`items[${i}]`}
-				type="checkbox"
-				role="checkbox"
-				aria-checked={done}
-				bind:checked={done}
-			/>
-			<input
-				data-testid="input-item-text"
-				name={`items[${i}]`}
-				type="text"
-				placeholder="Item"
-				class:done
-				bind:value={desc}
-				on:keydown={(event) => onItemKeydown(event, desc, i)}
-				use:onItemMount
-			/>
-			<button
-				data-testid="button-item-del"
-				aria-label="Remove item"
-				on:click|preventDefault={() => removeItem(i)}>✕</button
-			>
+					<input
+						id="input-title"
+						data-testid="input-title"
+						type="text"
+						name="title"
+						placeholder="Title"
+						bind:value={title}
+						class="card-title input input-ghost"
+					/>
+
+					{#each items as { done, desc }, i}
+						<div class="item">
+							<input
+								data-testid="input-item-check"
+								name={`items[${i}]`}
+								type="checkbox"
+								role="checkbox"
+								class="checkbox"
+								aria-checked={done}
+								bind:checked={done}
+							/>
+							<input
+								data-testid="input-item-text"
+								name={`items[${i}]`}
+								type="text"
+								placeholder="Item"
+								class="input"
+								class:done
+								bind:value={desc}
+								on:keydown={(event) => onItemKeydown(event, desc, i)}
+								use:onItemMount
+							/>
+							<button
+								class="btn btn-circle btn-ghost"
+								data-testid="button-item-del"
+								aria-label="Remove item"
+								on:click|preventDefault={() => removeItem(i)}>✕</button
+							>
+						</div>
+					{/each}
+
+					<div class="new-item">
+						<input
+							type="checkbox"
+							class="checkbox"
+							name="new-item-check"
+							disabled
+						/>
+						<input
+							type="text"
+							class="input"
+							placeholder="New item"
+							bind:value={newItemDesc}
+						/>
+					</div>
+
+					<div class="card-actions justify-end">
+						<button
+							class="btn btn-ghost"
+							disabled={!isValid}
+							id="save-button"
+							formaction="?/save"
+						>
+							Save list
+						</button>
+
+						<button
+							class="btn btn-ghost"
+							id="delete-button"
+							formaction="?/delete"
+						>
+							Delete list
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
-	{/each}
-	<div class="new-item">
-		<input type="checkbox" name="new-item-check" disabled />
-		<input type="text" placeholder="New item" bind:value={newItemDesc} />
-	</div>
-
-	<button disabled={!isValid} id="save-button" formaction="?/save">
-		Save list
-	</button>
-	<button id="delete-button" formaction="?/delete"> Delete list </button>
-</form>
+	</form>
+</div>
