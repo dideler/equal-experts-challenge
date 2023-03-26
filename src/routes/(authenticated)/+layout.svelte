5da28e3bbd535819e0345fb610ae69b9c27189e5
Dennis Ideler <ideler.dennis@gmail.com>
2023-03-26 20:33:10 +0100

    Change link and text of navbar home button

--- src/routes/(authenticated)/+layout.svelte
+++ src/routes/(authenticated)/+layout.svelte
@@ -1,5 +1,5 @@
 <div class="navbar bg-base-300">
-	<a class="btn btn-ghost normal-case text-xl" href="/">Lists</a>
+	<a class="btn btn-ghost normal-case text-xl" href="/lists">Grocery List App</a>
 </div>
 
 <slot />
